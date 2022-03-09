396c7e2ba1110d3634588d10a27c9a586443d711
Thanseef <androtsf@gmail.com>
2022-03-09 16:32:00 +0530

    project initialised

---
+++ StudentData/Controllers/SudentsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SudentsTableViewController.swift
+//  StudentData
+//
+//  Created by THANSEEF on 09/03/22.
+//
+
+import UIKit
+
+class SudentsTableViewController: UITableViewController {
+
+    @IBOutlet var studentListTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
---
+++ StudentData/Model/StudentModel.swift
@@ -0,0 +1,13 @@
+//
+//  StudentModel.swift
+//  StudentData
+//
+//  Created by THANSEEF on 09/03/22.
+//
+
+import Foundation
+
+
+struct StudentModel {
+//    @objc dynamic var name : String = ""
+}
